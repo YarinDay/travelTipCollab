bc9b3fa4ac07faf22740147c47ea4ec1c679db1b
Beker <orbeker7@gmail.com>
2022-08-17 15:35:21 +0300

    Changes have been made

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -1,14 +1,16 @@
+
+// import { storageService } from './services/storage.service.js'
+
 export const mapService = {
     initMap,
     addMarker,
     panTo
 }
 
-
 // Var that is used throughout this Module (not global)
 var gMap
 
-function initMap(lat = 32.0749831, lng = 34.9120554) {
+function initMap(lat = 31.501595418345833, lng = 34.46217911117168) {
     console.log('InitMap')
     return _connectGoogleApi()
         .then(() => {
@@ -19,7 +21,47 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
                 zoom: 15
             })
             console.log('Map!', gMap)
+            let locs = {
+                lat,
+                lng
+            }
+            startMap(gMap, locs)
         })
+}
+
+function startMap(map, myLatlng) {
+    console.log('myLatlng : ',myLatlng);
+    // const myLatlng = { pos.lat: -25.363, pos.lng: 131.044 };
+
+    // const map = new google.maps.Map(document.getElementById("#map")!, {
+    //     zoom: 4,
+    //     center: myLatlng,
+    // });
+
+    // Create the initial InfoWindow.
+    let infoWindow = new google.maps.InfoWindow({
+        content: "Click the map to get Lat/Lng!",
+        position: myLatlng
+    });
+    console.log('position : ',position);
+    infoWindow.open(map);
+
+    // Configure the click listener.
+    map.addListener("click", (mapsMouseEvent) => {
+        // Close the current InfoWindow.
+        infoWindow.close();
+
+        // Create a new InfoWindow.
+        infoWindow = new google.maps.InfoWindow({
+            //SAVE
+            position: mapsMouseEvent.latLng,
+        });
+        console.log('infoWindow!!! : ',infoWindow);
+        infoWindow.setContent(
+            JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+        );
+        infoWindow.open(map);
+    });
 }
 
 function addMarker(loc) {
@@ -33,13 +75,13 @@ function addMarker(loc) {
 
 function panTo(lat, lng) {
     var laLatLng = new google.maps.LatLng(lat, lng)
+    console.log('laLatLng : ', laLatLng);
     gMap.panTo(laLatLng)
 }
 
-
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = 'AIzaSyAONBuSkGU1JpdVw8GEKJhkzB0X8bsHsMU' //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyAONBuSkGU1JpdVw8GEKJhkzB0X8bsHsMU' //DONE: Enter your API Key
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
---
+++ js/services/storage.service.js
@@ -0,0 +1,18 @@
+
+export const storageService = {
+    saveToStorage,
+    loadFromStorage
+}
+
+        function saveToStorage(key, val) {
+            console.log('key : ',key);
+            console.log('val : ',val);
+            const str = JSON.stringify(val)
+            localStorage.setItem(key, str)
+        }
+        
+        function loadFromStorage(key) {
+            const str = localStorage.getItem(key)
+            const val = JSON.parse(str)
+            return val
+        }
